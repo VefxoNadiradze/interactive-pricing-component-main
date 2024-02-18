cb0cfb685fb3922ef8c4954f4e2745adb0b7c045
VefxoNadiradze <olld00@bk.ru>
2024-02-18 22:49:37 +0400

    add switch button functional also money and pageviewers

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const views = document.querySelector(".views");
+const slider = document.querySelector(".range");
+const money = document.querySelector(".moneySpan");
+const check = document.querySelector(".check");
 
+const pageView = ["10K", "50K", "100K", "500K", "1M"];
+const preMonth = [8, 12, 16, 24, 36];
+
+slider.addEventListener("input", () => {
+  views.innerHTML = pageView[slider.value];
+  money.innerHTML = preMonth[slider.value];
+});
