b1c1b4fd307e9f27f9835301478e977c828d98f9
VefxoNadiradze <olld00@bk.ru>
2024-02-19 17:12:21 +0400

    add switch function for yearly and monthly payment

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const views = document.querySelector(".views");
 const slider = document.querySelector(".range");
 const money = document.querySelector(".moneySpan");
 const check = document.querySelector(".check");
+const Switch = document.querySelector(".switch");
 
 const pageView = ["10K", "50K", "100K", "500K", "1M"];
 const preMonth = [8, 12, 16, 24, 36];
@@ -9,4 +10,23 @@ const preMonth = [8, 12, 16, 24, 36];
 slider.addEventListener("input", () => {
   views.innerHTML = pageView[slider.value];
   money.innerHTML = preMonth[slider.value];
+  if (Switch.classList.contains("activeSwitch")) {
+    yearlyFun();
+  } else {
+    money.innerHTML = preMonth[slider.value];
+  }
 });
+
+Switch.addEventListener("click", () => {
+  Switch.classList.toggle("activeSwitch");
+
+  if (Switch.classList.contains("activeSwitch")) {
+    yearlyFun();
+  } else {
+    money.innerHTML = preMonth[slider.value];
+  }
+});
+
+function yearlyFun() {
+  money.innerHTML = preMonth[slider.value] * 12 * 0.75;
+}
